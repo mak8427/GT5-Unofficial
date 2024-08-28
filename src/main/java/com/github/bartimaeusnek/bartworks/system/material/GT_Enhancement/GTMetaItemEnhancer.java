23a81935b4c509437f0ba009bcc2485a77cd25cb
boubou19 <miisterunknown@gmail.com>
2024-08-29 01:00:56 +0200

    Revert "Spotless apply for branch fix/capsule_hiding for #2993 (#2994)"
    This reverts commit 2edb30e4106f199651f435590dad0031093e2d4d.

--- src/main/java/com/github/bartimaeusnek/bartworks/system/material/GT_Enhancement/GTMetaItemEnhancer.java
+++ src/main/java/com/github/bartimaeusnek/bartworks/system/material/GT_Enhancement/GTMetaItemEnhancer.java
@@ -13,6 +13,7 @@
 
 package com.github.bartimaeusnek.bartworks.system.material.GT_Enhancement;
 
+import static gregtech.api.enums.Mods.BartWorks;
 import static gregtech.api.enums.Mods.Forestry;
 import static gregtech.api.recipe.RecipeMaps.fluidCannerRecipes;
 import static gregtech.api.util.GT_RecipeBuilder.TICKS;
@@ -21,6 +22,7 @@ import static gregtech.api.util.GT_RecipeBuilder.WILDCARD;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import codechicken.nei.api.API;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.Fluid;
@@ -28,7 +30,6 @@ import net.minecraftforge.fluids.FluidContainerRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.oredict.OreDictionary;
 
-import codechicken.nei.api.API;
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
